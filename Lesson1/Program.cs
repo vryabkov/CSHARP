e54c2e522f90d225a9bb8d73402db9d2a82b7cea
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-04-16 11:58:40 +0500

    add some variables Lesson1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
