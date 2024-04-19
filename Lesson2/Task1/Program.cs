a94418d856900ca0d5ef7eeb46b9b4b56aabb0fa
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-04-19 21:28:43 +0500

    add Lessom2:task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr[4]); - нудопустимое значение вывода
+
+int[] arr2 = new arr[5] { 1, 2, 3, 4, 5 };
+
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
+// arr = {1,2,3,4,5}
