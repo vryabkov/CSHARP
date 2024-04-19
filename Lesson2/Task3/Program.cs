b17a4b160763a31e006da7180891a2e91bdc0aa9
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-04-19 22:21:55 +0500

    add Lesson2:task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 1, 2, 3, 5, 6, 7, 8, 9, 6, 3 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
