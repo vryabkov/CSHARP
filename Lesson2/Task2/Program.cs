619d9e60ac73ad232761ce771f4d95c6cc83647f
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-04-19 21:47:52 +0500

    add Lesson2:task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+//или так while (i < n)
+{
+    arr[i] = i + 1;
+    //console.Write(arr[i]);
+    //Console.WriteLine(' ');
+    Console.Write($"{arr[i]}");
+    i = i + 1;
+}
