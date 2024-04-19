4c348a03263bd39180b23f84b9bef06d469ee31c
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-04-19 22:47:48 +0500

    add Lesson2:task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿//Выводим наибольшее значение из массива через While
+
+int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{if (array[i] > max)
+{
+    max = array[i];
+}
+i = i + 1;
+}
+Console.WriteLine(max);
+
+//Выводим наибольшее значение из массива через for
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+   if (array[j] > max)
+{
+    max = array[j];
+}
+}
+Console.WriteLine(max);
+
+//Выводим наибольшее значение из массива через Foreach
+
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max)
+{
+    max = e;
+}
+}
+Console.WriteLine(max);
